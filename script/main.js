a5f952c11c1d40be26e26b317cd0a1c919635da3
qeahhn <qeahhn@gmail.com>
2025-11-11 12:56:31 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -12,7 +12,8 @@ window.addEventListener('load', () => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
             animationTimeline();
-        } else
+        } else (result.iscanceled) {
+          document.querySelector('.song').play();  
             animationTimeline();
         }
     });
